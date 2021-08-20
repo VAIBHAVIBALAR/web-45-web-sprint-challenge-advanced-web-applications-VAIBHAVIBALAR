c14886f8aa09a848c4fb70b12b200fa3bfae41d5
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-08-19 18:03:56 -0700

    initiallogincommit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -13,6 +13,14 @@ function App() {
           <a data-testid="logoutButton" href="#">logout</a>
         </header>
       </div>
+      <Switch>
+        <Route exact path="/">
+          <Login/>
+        </Route>
+        <Route exact path="/login">
+           <Login/> 
+        </Route>
+      </Switch>
     </Router>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,50 @@
-import React from "react";
+import axios from "axios";
+import React, {useState} from "react";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [auth, setAuth] = useState({
+    username :"",
+    password: ""
+  })
 
-  const error = "";
+  const [error, setError] = useState("")
   //replace with error state
+  const { push } = useHistory();
+  const changeHandler = (e) =>{
+    setAuth({
+      ...auth,
+      [e.target.name] : e.target.value
+    })
+
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', auth)
+    .then(res =>{
+      console.log(res)
+      localStorage.getItem("token", res.data.payload);
+      push('/bubblepage')
+
+    })
+
+  }
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <h2>Login Form</h2>
+        <form>
+          <label>Username</label>
+          <input id="username" name="username" type="text"  onChange={changeHandler} value={auth.username}/>
+          <label>Password</label>
+          <input id="password" name="password" type="password" onChange={changeHandler} value={auth.password}/>
+          <button id="submit" onClick={handleSubmit}>Submit</button>
+        </form>
       </div>
 
       <p id="error" className="error">{error}</p>
